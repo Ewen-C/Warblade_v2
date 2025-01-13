c34b9e2d8e365664a8d2dccc50834f44eb00f9c4
Ewen-C <ewen.celibert@gmail.com>
2025-01-13 11:19:36 +0100

    Shoot on input w/ strict cooldown

--- scenes/player_ship/ship.gd
+++ scenes/player_ship/ship.gd
@@ -6,14 +6,25 @@ extends Node2D
 @onready var cannon_timer: Timer = $CannonTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 
-var can_fire = false
+var can_shoot = true
+var input_shoot = false
 
-func _ready() -> void:
-	cannon_timer.timeout.connect(fire_lasers)
-
-func fire_lasers() -> void:
+func _input(event):
+	if event.is_action_pressed("shoot"): 
+		input_shoot = true
+		shoot_lasers()
+	elif event.is_action_released("shoot"): 
+		input_shoot = false
+	
+func shoot_lasers() -> void:
+	if not can_shoot: return
+	can_shoot = false
+	cannon_timer.start()
+	
 	spawner_component.spawn(left_cannon.global_position)
 	spawner_component.spawn(right_cannon.global_position)
 	scale_component.tween_scale()
-	
-	#Input.is_action_pressed("shoot")
+
+func _on_cannon_timer_timeout() -> void:
+	can_shoot = true
+	if input_shoot: shoot_lasers()
